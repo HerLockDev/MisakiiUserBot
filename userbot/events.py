7c182b2fa073721b1d2b2d7f6d3c7f3046d74f86
HerLockDev <94521956+HerLockDev@users.noreply.github.com>
2021-11-20 20:02:15 +0400

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -78,9 +78,9 @@ def register(**args):
             except BaseException:
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
-                    text = "**BOZQURD XETA HESABATI**\n"
-                    link = "[BozQurd](https://t.me/bozqurdsup)"
+N
+                    text = "**MISAKI XETA HESABATI**\n"
+                    link = "[MISAKI](https://t.me/MisakiSupport)"
                     text += "İstəsəniz bunu şikayət edə bilərsiniz"
                     text += f" - sadəcə bu mesajı buraya göndərin {link}.\n"
                     text += "Xəta və Tarixdən başqa heçnə qeyd edilmir\n"
@@ -92,7 +92,7 @@ def register(**args):
                     ftext += "\nburada hansısa şexsi melumatınız varsa"
                     ftext += "\nbu xeta hesabatıb olmaya biler, kimse melumatlarınıza baxa bilmez.\n"
                     ftext += "================================\n\n"
-                    ftext += "--------BOZQURD XETA HESABATI--------\n"
+                    ftext += "--------MISAKI XETA HESABATI--------\n"
                     ftext += "\nTarix: " + date
                     ftext += "\nQrup ID: " + str(check.chat_id)
                     ftext += "\nGönderen İsdifadeçinin ID: " + str(check.sender_id)
@@ -102,7 +102,7 @@ def register(**args):
                     ftext += str(format_exc())
                     ftext += "\n\nXeta:\n"
                     ftext += str(sys.exc_info()[1])
-                    ftext += "\n\n--------BOZQURD XETA HESABATI SONLUQ--------"
+                    ftext += "\n\n--------MISAKI XETA HESABATI SONLUQ--------"
 
                     command = "git log --pretty=format:\"%an: %s\" -10"
 
